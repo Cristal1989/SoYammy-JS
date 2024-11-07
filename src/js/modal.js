e944cb6a67a7411fca6399b2de357dda69c392a4
Cristal1989 <gips5921@gmail.com>
2024-11-07 17:25:58 +0200

    1

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-modal-open1]'),
+    closeModalBtn: document.querySelector('[data-modal-close1]'),
+    modal: document.querySelector('[data-modal1]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal1);
+  refs.closeModalBtn.addEventListener('click', toggleModal1);
 
-  function toggleModal() {
+  function toggleModal1() {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
