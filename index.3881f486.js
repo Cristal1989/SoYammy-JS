ddb53903d09bff6bbb0ac63d85a006e7cbf60a5a
Cristal1989 <151545204+Cristal1989@users.noreply.github.com>
2024-11-08 14:28:57 +0000

    Deploying to gh-pages from @ Cristal1989/SoYammy-JS@a1ed097fc4f24f5cd95eb513dbb982dc37221fd4 🚀

---
+++ index.3881f486.js
@@ -0,0 +1,2 @@
+(()=>{let o={openModalBtn:document.querySelector("[data-modal-open-logout]"),closeModalBtn:document.querySelector("[data-modal-close-logout]"),modal:document.querySelector("[data-modal-logout]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.3881f486.js.map
---
+++ index.6d2a5885.js
@@ -0,0 +1,2 @@
+(()=>{let e={openModalBtn:document.querySelector("[data-modal-open-edit]"),closeModalBtn:document.querySelector("[data-modal-close-edit]"),modal:document.querySelector("[data-modal-edit]")};function d(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",d),e.closeModalBtn.addEventListener("click",d)})();
+//# sourceMappingURL=index.6d2a5885.js.map
